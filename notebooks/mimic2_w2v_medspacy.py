6d9427f966ee6211cc8ed5488dd7c1705686786f
Brian Chapman <chapmanbe@gmail.com>
2020-08-13 11:33:01 +1000

    added scripts for generating word vectors from mimic2 and mimic3

--- notebooks/mimic2_w2v_medspacy.py
+++ notebooks/mimic2_w2v_medspacy.py
@@ -4,7 +4,7 @@ import spacy
 from gensim import utils
 import gensim.models
 import sys
-
+NUM_REPORTS = 500000
 MIMICHOST = "35.233.174.193"
 
 def get_mimic_connection():
@@ -18,11 +18,10 @@ cursor = conn.cursor()
 
 print("loading i2b2 language model")
 
-nlp = spacy.load("en_info_3700_i2b2_2012", disable=["tagger", "parser", "ner"])
-nlp.add_pipe(nlp.create_pipe('sentencizer'))
+nlp = medspacy.load("en_info_3700_i2b2_2012", disable=["tagger", "parser", "ner", "target_matcher",
+                                                       "sectionizer", "context", "postprocessor"])
 print(nlp.pipeline)
-
-cursor.execute("""SELECT text FROM noteevents WHERE category='RADIOLOGY_REPORT' LIMIT 200000""")
+cursor.execute("""SELECT text FROM noteevents WHERE category='RADIOLOGY_REPORT' LIMIT %d"""%NUM_REPORTS)
 r = [r[0] for r in cursor.fetchall()]
 print(len(r))
 r = [rr for rr in r if rr]
@@ -36,5 +35,5 @@ print(len(sents))
 print('training model')
 model = gensim.models.Word2Vec(sentences=sents, min_count=20, size=1000, workers=6)
 print('saving model')
-model.save("mimic2wv_b_radiology_spacy.gz")
+model.save("mimic2wv_b_radiology_medspacy.gz")
 
